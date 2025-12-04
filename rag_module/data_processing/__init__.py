aad715efb95078f75ebf624eef29846d787ab7e5
Rahimov Imran <mr.rahimov.imran@gmail.com>
2025-12-04 12:20:24 +0400

    feat: reorganize data processing module by removing analyzers and updating imports

--- rag_module/data_processing/__init__.py
+++ rag_module/data_processing/__init__.py
@@ -1,8 +1,18 @@
 """Data processing pipeline components for RAG module."""
 
-from .pipeline import DocumentProcessingPipeline, PipelineFactory
+from .analyzers.base import BaseTextAnalyzer, DummyAnalyzer, NewsClassifier
+from .chunkers import FixedSizeChunker, SentenceChunker
+from .cleaners import AzerbaijaniNewsCleaner
+from .loaders import JSONFileLoader, TelegramJSONLoader
+from .pipeline import (
+    AsyncDocumentProcessingPipeline,
+    AsyncPipelineFactory,
+    DocumentProcessingPipeline,
+    PipelineFactory,
+)
 from .protocols import (
     Document,
+    IAsyncTextAnalyzer,
     IDataLoader,
     ITextAnalyzer,
     ITextChunker,
@@ -10,11 +20,29 @@ from .protocols import (
 )
 
 __all__ = [
+    # Protocols
     "Document",
     "IDataLoader",
     "ITextCleaner",
     "ITextAnalyzer",
+    "IAsyncTextAnalyzer",
     "ITextChunker",
+    # Analyzers
+    "BaseTextAnalyzer",
+    "DummyAnalyzer",
+    "NewsClassifier",
+
+    # Chunkers
+    "FixedSizeChunker",
+    "SentenceChunker",
+    # Cleaners
+    "AzerbaijaniNewsCleaner",
+    # Loaders
+    "JSONFileLoader",
+    "TelegramJSONLoader",
+    # Pipeline
     "DocumentProcessingPipeline",
     "PipelineFactory",
+    "AsyncDocumentProcessingPipeline",
+    "AsyncPipelineFactory",
 ]
