21ed0dbf7e593c3592d1bc67cdceb1ea77df31cb
Rahimov Imran <mr.rahimov.imran@gmail.com>
2025-12-31 15:11:38 +0400

    fix: update Celery backend configuration to use DATABASE_URL and enhance result backend options

--- backend/src/tasks/celery_app.py
+++ backend/src/tasks/celery_app.py
@@ -6,11 +6,16 @@ from celery import Celery
 from celery.schedules import crontab
 
 redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
+database_url = os.getenv(
+    "DATABASE_URL",
+)
 
 celery_app = Celery(
     "searchnewsrag",
     broker=redis_url,
-    backend=redis_url,
+    backend=database_url.replace(
+        "postgresql+psycopg", "db+postgresql+psycopg"
+    ),
     include=["tasks.news_tasks"],
 )
 
@@ -18,6 +23,8 @@ celery_app.conf.update(
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
+    result_backend_transport_options={"master_name": "mymaster"},
+    result_expires=86400,
     timezone="Asia/Baku",
     enable_utc=True,
     task_track_started=True,
@@ -25,6 +32,7 @@ celery_app.conf.update(
     task_soft_time_limit=3300,
     worker_prefetch_multiplier=1,
     worker_max_tasks_per_child=1000,
+    result_extended=True,
     beat_schedule={
         "daily-news-update": {
             "task": "tasks.news_tasks.daily_news_update",
